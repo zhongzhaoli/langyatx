2420d1a252e89eaa9a6b433808be07b3286a72fc
Custer <525966315@qq.com>
2019-03-25 23:56:08 +0800

    全部

--- js/shopping.js
+++ js/shopping.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
     $("[data-detail]").on("click", function () {
         var id = $(this).data('id');
         var eml = "a[href='#" + id + "']";
-        $(eml).tab("show");
-        $('body,html').animate({
-            scrollTop: 1100,
-        }, 700);
+        $(eml).tab("show")
+        setTimeout(function () {
+            $('body,html').animate({
+                scrollTop: $("#offset_top").offset().top,
+            }, 700);
+        })
     })
 })
