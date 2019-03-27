f298d9819a406e2c5be4c703c7f1ab57cfcacb8f
Custer <525966315@qq.com>
2019-03-28 00:55:19 +0800

    dasdasd

--- js/base.js
+++ js/base.js
@@ -1,3 +1,9 @@
 window.onclick = function(){
     $("audio")[0].play();
 }
+
+var img = new Image();
+img.onload = function(){
+	// $(".hide").removeClass("hide");
+}
+img.src = "images/silder1.jpg";
