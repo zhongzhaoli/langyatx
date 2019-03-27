06596f18ddd030e78cd54bb9a858cd935326ef44
Custer <525966315@qq.com>
2019-03-28 01:05:48 +0800

    dasdasd

--- js/base.js
+++ js/base.js
@@ -4,6 +4,10 @@ window.onclick = function(){
 
 var img = new Image();
 img.onload = function(){
-	// $(".hide").removeClass("hide");
+	$(".hide").removeClass("hide");
+	var len = $(".base_div img").length;
+	for(var i = 0; i < len; i++){
+		$(".base_div img")[i].src = "./images/fc_" + parseInt(i + 1) + ".png"
+	}
 }
-img.src = "images/silder1.jpg";
+img.src = "images/slider1.jpg";
